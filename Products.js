2c7fd486aaa40af7b9cbac2766d61b8e3b509a1b
luchogalli <36230321+luchogalli@users.noreply.github.com>
2023-12-09 17:08:43 -0300

    funca todo

--- Products.js
+++ Products.js
@@ -1,85 +1,89 @@
 import { Router } from "express";
+import fs from 'fs/promises';
 
 const productsRouter = Router();
-let products = [
-    {
-        id: 1
-    },
-    {
-        id: 2
-    },
-    {
-        id: 3
-    },
-    {
-        id: 4
-    }
-];
+const PRODUCTS_FILE = 'products.json';
 
-productsRouter.get("/", (request, response) => {
-    const {limit} = request.query;
+async function readProducts() {
+    try {
+        const data = await fs.readFile(PRODUCTS_FILE, 'utf8');
+        return JSON.parse(data);
+    } catch (err) {
+        console.error('Error reading file:', err);
+        return [];
+    }
+}
+
+async function writeProducts(products) {
+    try {
+        await fs.writeFile(PRODUCTS_FILE, JSON.stringify(products, null, 2), 'utf8');
+    } catch (err) {
+        console.error('Error writing file:', err);
+    }
+}
+
+productsRouter.get("/", async (request, response) => {
+    const { limit } = request.query;
+    const products = await readProducts();
 
     let productsToShow = [];
     const showAllProducts = !limit;
 
-    if(showAllProducts){
+    if (showAllProducts) {
         productsToShow = products;
-    }
-    else{
-        for (let i = 0; i < products.length && i <= parseInt(limit) - 1; i++) {
-            const element = products[i];
-            productsToShow.push(element);
-        }
+    } else {
+        productsToShow = products.slice(0, parseInt(limit));
     }
 
     response.send(productsToShow);
 });
 
-productsRouter.get("/:id", (request, response) => {
+productsRouter.get("/:id", async (request, response) => {
     const { id } = request.params;
-    const element = products.find(item => item.id === parseInt(id));
+    const products = await readProducts();
+    const product = products.find(item => item.id === parseInt(id));
 
-    if(!element){
-        return response.send("No product of id " + id + " found");
+    if (!product) {
+        return response.status(404).send("No product of id " + id + " found");
     }
 
-    response.send(element);
+    response.send(product);
 });
 
-productsRouter.post("/", (request, response) => {
-    const product = request.body;
+productsRouter.post("/", async (request, response) => {
+    const newProduct = request.body;
+    const products = await readProducts();
     const id = products.length + 1;
-    products.push({...product, id});
+    products.push({ ...newProduct, id });
+    await writeProducts(products);
     response.send("Product added.");
 });
 
-productsRouter.put("/:id", (request, response) => {
-    const {id} = request.params;
+productsRouter.put("/:id", async (request, response) => {
+    const { id } = request.params;
+    const products = await readProducts();
 
-    if (!products.some(item => item.id === parseInt(id))){
-        response.send({error:"SE FUNO " + id});
+    if (!products.some(item => item.id === parseInt(id))) {
+        return response.status(404).send({ error: "No product of id " + id + " found" });
     }
 
     const bodyProduct = request.body;
     const updatedProducts = products.map(product => {
-        if(product.id === parseInt(id)){
-            return {
-                ...product,
-                ...bodyProduct,
-                id: parseInt(id)
-            }
+        if (product.id === parseInt(id)) {
+            return { ...product, ...bodyProduct, id: parseInt(id) };
         }
-
         return product;
     });
 
+    await writeProducts(updatedProducts);
     response.send("Updated product");
-    products = updatedProducts;
 });
 
-productsRouter.delete("/:id", (request, response) => {
-    const{id} = request.params;
-    products = products.filter(product => product.id !== parseInt(id));
+productsRouter.delete("/:id", async (request, response) => {
+    const { id } = request.params;
+    const products = await readProducts();
+    const filteredProducts = products.filter(product => product.id !== parseInt(id));
+    await writeProducts(filteredProducts);
     response.send(id + " deleted");
 });
 
